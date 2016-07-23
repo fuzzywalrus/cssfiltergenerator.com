3ac7186e40be24cb74292eb97ba44d322a390c28
Greg Gant <greg@emergeinteractive.com>
2016-07-23 13:22:33 -0700

    Reset + no-js

--- js/app/app.js
+++ js/app/app.js
@@ -1,107 +1,110 @@
 (function(jQuery) {
-    "use strict";
-    var Engine;
+  "use strict";
+  var Engine;
     jQuery(document).ready(function() {
       Engine = {
         ui: {
-
-			sliders : function () {
-				function slider(MySliderName){
-					var filtername = $(MySliderName).data("filter");
-					var MySliderNameA = "#" + filtername + "-a";
-					var MySliderNameB ="#" +  filtername + "-b";
-					//slider change
-					$( MySliderNameA ).change(function() {
-						var myval = $(this).val();
-						$(MySliderNameB).val(myval);
-						console.log("myval: " + myval);
-					});
-					//number change
-					$(MySliderNameB).change(function() {
-						var myval = $(this).val();
-						$(MySliderNameA).val(myval);
-					});
-				}
-				$( "input" ).each(function() {
-					slider(this);
-				});
-
-			},
-			onchanges : function() {
-				$("#imageURL").change(function() {
-					var demoimage = $(this).val();
-					$("#demoimage").attr("src", demoimage);
-				});
-
-				$("input").change(function() {
-					var filters = "";
-					var hoverstate = "";
-					$( "input[type=range]" ).each(function() {
-						var myValue = $(this).val();
-					 	var filtername = $(this).data("filter");
-						var myDefaultVal  = $(this).attr('value');
-
-				    var concatme =  filtername + "("+ myValue + ") ";
-						var concatmehover = filtername + "(" + myDefaultVal + ") ";
-
-						if ($(this).is(':disabled') === false) {
-							console.log("concat:" + concatme);
-						filters = filters + concatme;
-						hoverstate = hoverstate + concatmehover;
+					sliders : function () {
+						function slider(mySliderName){
+							//follow naming scheme
+							var filterName = $(mySliderName).data("filter");
+							var mySliderNameA = "#" + filterName + "-a";
+							var mySliderNameB ="#" +  filterName + "-b";
+							//slider change
+							$( mySliderNameA ).change(function() {
+								var myval = $(this).val();
+								$(mySliderNameB).val(myval);
+							});
+							//number change
+							$(mySliderNameB).change(function() {
+								var myval = $(this).val();
+								$(mySliderNameA).val(myval);
+							});
 						}
+						$( "input[data-filter]" ).each(function() {
+							slider(this);
+						});
+					},
+					onchanges : function() {
+						$("#imageURL").change(function() {
+							var demoimage = $(this).val();
+							$("#demoimage").attr("src", demoimage);
+						});
 
-				  });
+						$("input").change(function() {
+							var filters = "";
+							var hoverState = "";
+							$( "input[type=range]" ).each(function() {
+								var myValue = $(this).val();
+							 	var filterName = $(this).data("filter");
+								var myDefaultVal  = $(this).attr('value');
+								var dataAdditional = $(this).data("additional"); //
+								if (dataAdditional == undefined) {
+									dataAdditional = "";
+								}
+						    var concatMe =  filterName + "("+ myValue + dataAdditional +") ";
+								var concatMehover = filterName + "(" + myDefaultVal + ") ";
+								if ($(this).is(':disabled') === false) {
+									filters = filters + concatMe;
+									hoverState = hoverState + concatMehover;
+								}
+						  });
+							// ugly writes to DOM
+							$("#filter").html("filter: " + filters + "; <p> -webkit-filter: " + filters +  ";  <p> -moz-filter: " + filters + ";");
+							$("#inlinestyle").html("<style> img#demoimage:hover {"+ "filter: " + hoverState + " !important; -webkit-filter: " + hoverState + " !important;  -moz-filter: " + hoverState + " !important;" +"}</style>")
+							$("#demoimage").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
+							$("img[data-fullsize]").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
+						});
+					},
+					newimage : function (){
+						$("img[data-fullsize]").click(function() {
+								var newUrl = $(this).data("fullsize");
+								$("#demoimage").attr("src", newUrl);
+						});
+					},
+					showhidefilters : function (){
+						$("label").click(function() {
+							var myLabel = $(this);
+							var myFilter = $(this).data("filter");
+							myFilter = "input[data-filter="+ myFilter  + "]";
 
-					$("#filter").html("filter: " + filters + "; <p> -webkit-filter: " + filters +  ";  <p> -moz-filter: " + filters + ";");
-					$("#inlinestyle").html("<style> img#demoimage:hover {"+ "filter: " + hoverstate + " !important; -webkit-filter: " + hoverstate + " !important;  -moz-filter: " + hoverstate + " !important;" +"}</style>")
-					$("#demoimage").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
-					$("img[data-fullsize]").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
-				});
-
-			},
-			newimage : function (){
-				$("img[data-fullsize]").click(function() {
-						var newurl = $(this).data("fullsize");
-						$("#demoimage").attr("src", newurl);
-				});
-			},
-			showhidefilters : function (){
-				$("label").click(function() {
-					var mylabel = $(this);
-					var myfilter = $(this).data("filter");
-					myfilter = "input[data-filter="+ myfilter  + "]";
-
-					if ($(myfilter).is(':disabled') === true) {
-						$(myfilter).attr("disabled", false);
-						 $("#sepia-a").change();
-						 $(mylabel).removeClass("disabled");
-					} else {
-						$(myfilter).attr("disabled", true);
-						$(mylabel).addClass("disabled");
-						 $("#sepia-a").change();
-					}
-				});
-			},
-
-			sorting : function(){
-				$( "#sortable" ).sortable({
-					axis: "y",
-					containment:  "#contain",
-					scroll: false,
-					stop:  function(event, ui) {
-						$("#sepia-a").change();
-				 }
-				});
-			}
-
-
+							if ($(myFilter).is(':disabled') === true) {
+								$(myFilter).attr("disabled", false);
+								 $("#sepia-a").change();
+								 $(myLabel).removeClass("disabled");
+							} else {
+								$(myFilter).attr("disabled", true);
+								$(myLabel).addClass("disabled");
+								 $("#sepia-a").change();
+							}
+						});
+					},
+					reset : function() {
+						$("#reset").click(function() {
+							$( "input[data-filter]" ).each(function() {
+								var defaultVal = $(this).attr('value');
+								$(this).val(defaultVal);
+							});
+								$("#sepia-a").change();
+						});
+					},
+					sorting : function(){
+						$( "#sortable" ).sortable({
+							axis: "y",
+							containment:  "#contain",
+							scroll: false,
+							stop:  function(event, ui) {
+								$("#sepia-a").change();
+						 }
+					 });
+				}
 		} // ui
 	}; // Engine
-
 	Engine.ui.sliders();
 	Engine.ui.onchanges();
 	Engine.ui.sorting();
 	Engine.ui.showhidefilters();
+	Engine.ui.reset();
 });
 }(jQuery));
 
