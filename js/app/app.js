14ecc869655f1c107d3028756fef331457d5807a
Greg Gant <greg@emergeinteractive.com>
2016-07-26 00:12:33 -0700

    V0.5 - Presets include gradient support! More demo images!
    Massive update for gradient support

--- js/app/app.js
+++ js/app/app.js
@@ -51,6 +51,10 @@
 							$("#inlinestyle").html("<style> #filter-wrapper:hover {"+ "filter: " + hoverState + " !important; -webkit-filter: " + hoverState + " !important;  -moz-filter: " + hoverState + " !important;" +"}</style>")
 							$("#filter-wrapper").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
 							$("img[data-fullsize]").attr("style", "filter: " + filters + "; -webkit-filter: " + filters + ";  -moz-filter: " + filters + ";");
+							if ( $("#overlay-radio-none").prop("checked") ) {
+								$("#filter .overlay-css").html("");
+							   $("#overlay-css").html("");
+							}
 						});
 					},
 					newimage : function (){
@@ -59,7 +63,7 @@
 								var newUrl = $(this).data("fullsize");
 								$("#demoimage").attr("src", newUrl);
 								$(".preset img").attr("src", newUrl);
-									console.log(newUrl);
+								console.log(newUrl);
 						});
 						//Replace the SRC of the demo image with the new URL to image
 						$("#imageURL").change(function() {
@@ -92,6 +96,8 @@
 							$( "input[data-filter]" ).each(function() {
 								var defaultVal = $(this).attr('value');
 								$(this).val(defaultVal);
+                Engine.ui.killOverlay();
+                $("#overlay-radio-none").prop("checked", true);
 							});
 								$("#sepia-a").change();
 						});
@@ -100,14 +106,22 @@
 						//New overlay functionality
 						$('input[type=radio][name=overlay]').change(function() {
 							var myChecked = $("input[type=radio][name=overlay]:checked").val();
-							if ( myChecked == "none") {
-								$('.overlay-group').hide();
+							if ( myChecked == "#overlay-radio-none") {
+								Engine.ui.killOverlay();
 							} else {
 								$('.overlay-group').hide();
 								$(myChecked).show();
+								$("#blending-mode").show();
+								$(".overlay-group input:visible").change();
 							}
 						});
 					},
+          killOverlay : function (){
+            $('.overlay-group').hide();
+            $("#filter .overlay-css").html("");
+            $("#overlay-css").html("");
+            $("#blending-mode").hide();
+          },
 					sorting : function(){
 						//jQuery UI init
 						$( "#sortable" ).sortable({
@@ -126,22 +140,86 @@
 					    showInput: true,
 							showAlpha: true,
 					    showPalette: false,
-							color: 'rgba(112,55,200, 0.2)',
-							move: function (color) { updateColor("#overlay-solid-color-text", color); },
-							hide: function (color) { updateColor("#overlay-solid-color-text", color); }
+							color: 'rgba(112,55,200, 0.4)',
+							move: function (color) { Engine.ui.updateColor("#overlay-solid-color-text", color); },
+							hide: function (color) { Engine.ui.updateColor("#overlay-solid-color-text", color); }
 						});
-						function updateColor(element, color) {
-			 				$(element).val( (color ? color : "") );
-							$(element).change();
-						}
+
+						//overlay-linear-gradient color 1
+						$('#overlay-linear-gradient-color1').spectrum({
+							preferredFormat: "rgb",
+					    showInput: true,
+							showAlpha: true,
+					    showPalette: false,
+							color: 'rgba(255, 0, 134, 0.4)',
+							move: function (color) { Engine.ui.updateColor("#overlay-linear-gradient-color1-text", color); },
+							hide: function (color) { Engine.ui.updateColor("#overlay-linear-gradient-color1-text", color); }
+						});
+						//overlay-linear-gradient color 2
+						$('#overlay-linear-gradient-color2').spectrum({
+							preferredFormat: "rgb",
+					    showInput: true,
+							showAlpha: true,
+					    showPalette: false,
+							color: 'rgba(16, 255, 0, 0.4)',
+							move: function (color) { Engine.ui.updateColor("#overlay-linear-gradient-color2-text", color); },
+							hide: function (color) { Engine.ui.updateColor("#overlay-linear-gradient-color2-text", color); }
+						});
+
+						//overlay-linear-gradient color 1
+						$('#overlay-radial-gradient-color1').spectrum({
+							preferredFormat: "rgb",
+					    showInput: true,
+							showAlpha: true,
+					    showPalette: false,
+							color: 'rgba(100, 10, 10, 0.9)',
+							move: function (color) { Engine.ui.updateColor("#overlay-radial-gradient-color1-text", color); },
+							hide: function (color) { Engine.ui.updateColor("#overlay-radial-gradient-color1-text", color); }
+						});
+						//overlay-linear-gradient color 2
+						$('#overlay-radial-gradient-color2').spectrum({
+							preferredFormat: "rgb",
+					    showInput: true,
+							showAlpha: true,
+					    showPalette: false,
+							color: 'rgba(83, 10, 100, 0.9)',
+							move: function (color) { Engine.ui.updateColor("#overlay-radial-gradient-color2-text", color); },
+							hide: function (color) { Engine.ui.updateColor("#overlay-radial-gradient-color2-text", color); }
+						});
+
+
 						$('.overlay-solid-color').change(function() {
-							console.log("Yes");
+							console.log("soild");
 							var myColor = $("#overlay-solid-color-text").val();
 							var myBlending = $("#blending-mode").val();
 							//ugly writes to the DOM
 							$("#filter .overlay-css").html("<p>" + ".myfilter {<p>&nbsp;&nbsp;&nbsp;position:relative; <p>} <p>.myfilter:after{ <p>&nbsp;&nbsp;&nbsp; content: ''; <p>&nbsp;&nbsp;&nbsp; display: block;  <p>&nbsp;&nbsp;&nbsp;  top: 0;  <p>&nbsp;&nbsp;&nbsp;  left: 0;  <p>&nbsp;&nbsp;&nbsp;  height: 100%;  <p>&nbsp;&nbsp;&nbsp;  width: 100%;  <p>&nbsp;&nbsp;&nbsp; position: absolute;  <p>&nbsp;&nbsp;&nbsp; background-color:" + myColor + ";  <p>&nbsp;&nbsp;&nbsp; mix-blend-mode: " + myBlending + "; <p> }");
 							myColor = "<style>#filter-wrapper:after{ background-color:" + myColor +"; mix-blend-mode: "  + myBlending + "; }</style>";
 							$("#overlay-css").html(myColor)
+						});
+
+						$('.overlay-linear-gradient-color').change(function() {
+							console.log("gradient");
+							var myColor1 = $("#overlay-linear-gradient-color1-text").val();
+							var myColor2 = $("#overlay-linear-gradient-color2-text").val();
+							var myBlending = $("#blending-mode").val();
+							var myGradient = "linear-gradient(to bottom,"+ myColor1 +" 0%, "+ myColor2 +" 100%);"
+							//ugly writes to the DOM
+							$("#filter .overlay-css").html("<p>" + ".myfilter {<p>&nbsp;&nbsp;&nbsp;position:relative; <p>} <p>.myfilter:after{ <p>&nbsp;&nbsp;&nbsp; content: ''; <p>&nbsp;&nbsp;&nbsp; display: block;  <p>&nbsp;&nbsp;&nbsp;  top: 0;  <p>&nbsp;&nbsp;&nbsp;  left: 0;  <p>&nbsp;&nbsp;&nbsp;  height: 100%;  <p>&nbsp;&nbsp;&nbsp;  width: 100%;  <p>&nbsp;&nbsp;&nbsp; position: absolute;  <p>&nbsp;&nbsp;&nbsp; background:" + myGradient + ";  <p>&nbsp;&nbsp;&nbsp; mix-blend-mode: " + myBlending + "; <p> }");
+							myGradient = "<style>#filter-wrapper:after{ background:" + myGradient +"; mix-blend-mode: "  + myBlending + "; }</style>";
+							$("#overlay-css").html(myGradient)
+						});
+
+						$('.overlay-radial-gradient-color').change(function() {
+							console.log("radial");
+							var myColor1x = $("#overlay-radial-gradient-color1-text").val();
+							var myColor2x = $("#overlay-radial-gradient-color2-text").val();
+							var myBlending = $("#blending-mode").val();
+							var myGradient = "radial-gradient(ellipse at center, " + myColor1x +" 0%, "+ myColor2x +" 100%);"
+							//ugly writes to the DOM
+							$("#filter .overlay-css").html("<p>" + ".myfilter {<p>&nbsp;&nbsp;&nbsp;position:relative; <p>} <p>.myfilter:after{ <p>&nbsp;&nbsp;&nbsp; content: ''; <p>&nbsp;&nbsp;&nbsp; display: block;  <p>&nbsp;&nbsp;&nbsp;  top: 0;  <p>&nbsp;&nbsp;&nbsp;  left: 0;  <p>&nbsp;&nbsp;&nbsp;  height: 100%;  <p>&nbsp;&nbsp;&nbsp;  width: 100%;  <p>&nbsp;&nbsp;&nbsp; position: absolute;  <p>&nbsp;&nbsp;&nbsp; background:" + myGradient + ";  <p>&nbsp;&nbsp;&nbsp; mix-blend-mode: " + myBlending + "; <p> }");
+							myGradient = "<style>#filter-wrapper:after{ background:" + myGradient +"; mix-blend-mode: "  + myBlending + "; }</style>";
+							$("#overlay-css").html(myGradient)
 						});
 				 },
 				 presetSet : function(filterName, newValue) {
@@ -158,6 +236,28 @@
 							$(mySliderNameB).val(newValue);
 					 }
 				 },
+				 gradientCheck : function(obj) {
+           // Used by presents to change the gradients
+					 var gradient = $(obj).data("gradient");
+           gradient = '[value="' + gradient + '"]';
+					 $("input[name=overlay]").filter(gradient).prop("checked", true)
+					 var color1 = $(obj).data("color1");
+					 var color2 = $(obj).data("color2");
+					 $(".overlay-group input.color1").val(color1);
+					 $(".overlay-group input.color2").val(color2);
+					 var blendingMode = $(obj).data("blending-mode");
+           $("#blending-mode").val(blendingMode);
+
+				 },
+         updateColorPicker : function(target, destination) {
+           $(target).change(function() {
+             $(destination).spectrum("set", $(target).val());
+           });
+         },
+         updateColor : function(element, color) {
+           $(element).val( (color ? color : "") );
+           $(element).change();
+         },
 				 presets : function() {
 					 $(".preset").click(function() {
 						 //eventually I'd like to simplify this to programmically cycle through the list of filters
@@ -173,7 +273,9 @@
 						 Engine.ui.presetSet("saturate", mySaturate);
 						 var mySepia = $(this).data("sepia");
 						 Engine.ui.presetSet("sepia", mySepia);
+						 Engine.ui.gradientCheck(this);
 						 $("#sepia-a").change();
+						 $("input[type=radio]").change();
 					 });
 				}
 		} // ui
@@ -187,6 +289,8 @@
 	Engine.ui.newimage();
 	Engine.ui.activeOverlay();
 	Engine.ui.colorPick();
+  Engine.ui.updateColorPicker(".overlay-group input.color1.text", ".overlay-group input.color1.picker");
+  Engine.ui.updateColorPicker(".overlay-group input.color2.text", ".overlay-group input.color2.picker");
 });
 }(jQuery));
 
